5aa8cf462834fd4754585a1da8971d43279007d5
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-01-28 18:35:50 -0300

    update profile formula adding social network
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- README.md
+++ README.md
@@ -4,11 +4,10 @@
 
 ## Documentation
 
-[Contribute to the Ritchie community](https://github.com/ZupIT/ritchie-formulas/blob/master/CONTRIBUTING.md)
+This repository contains Ritchie formulas which can be executed by [ritchie-cli](https://github.com/ZupIT/ritchie-cli).
 
-This repository contains Ritchie formulas which can be executed by the [ritchie-cli](https://github.com/ZupIT/ritchie-cli).
-
-- [Gitbook](https://docs.ritchiecli.io)
+- [Ritchie CLI documentation](https://docs.ritchiecli.io)
+- [Step by step to create a Github profile with Ritchie CLI](https://bit.ly/devtoritgithubcreateprofile)
 
 ## Formulas available on this repository
 
@@ -35,9 +34,10 @@ Finally, you can check if the repository has been imported correctly by executin
 
 1. Fork the repository
 2. Create a branch: `git checkout -b <branch_name>`
-3. Check the step by step of [how to create formulas on Ritchie](https://docs.ritchiecli.io/getting-started/creating-formulas)
+3. Check the step by step of [how to create formulas on Ritchie](https://docs.ritchiecli.io/tutorials/formulas/how-to-create-formulas)
 4. Add your formulas to the repository
 and commit your implementation: `git commit -m '<commit_message>`
 5. Push your branch: `git push origin <project_name>/<location>`
 6. Open a pull request on the repository for analysis.
 
+- [Contribute to Ritchie community](https://github.com/ZupIT/ritchie-formulas/blob/master/CONTRIBUTING.md)
--- github/create/profile/src/formula/formula.py
+++ github/create/profile/src/formula/formula.py
@@ -9,15 +9,35 @@ languages = '![Github Languages](https://github-readme-stats.vercel.app/api/top-
 statistics = '![Github Statistics](https://github-readme-stats.vercel.app/api/?username={}&count_private=true&show_icons=true)'
 contributions = '![Github Contributions](https://github-readme-streak-stats.herokuapp.com/?user={}&hide_border=true)'
 
-def Run(username, name, job, company):
+paragraph_init = '<p align="left">'
+paragraph_end = '</p>'
+
+linkedin = '<a href="{}"><img src="https://img.shields.io/badge/-LinkedIn-0077B5?style=flat&logo=Linkedin&logoColor=white"/></a>'
+twitter = '<a href="{}"><img src="https://img.shields.io/badge/-Twitter-%231DA1F2?style=flat&logo=twitter&logoColor=white"/></a>'
+medium = '<a href="{}"><img src="https://img.shields.io/badge/-Medium-%2312100E?style=flat&logo=medium&logoColor=white"/></a>'
+
+def Run(username, name, job, company, linkedin_url, twitter_url, medium_url):
     
     mdFile = MdUtils(file_name='README', title=title)
     
     mdFile.new_paragraph(resume.format(name, job, company))
     mdFile.new_paragraph(visitors.format(username, username))
+    
+    mdFile.new_header(level=3, title='Analytics ⚙️')
     mdFile.new_paragraph(languages.format(username))
     mdFile.new_paragraph(statistics.format(username))
     mdFile.new_paragraph(contributions.format(username))
+    
+    
+    mdFile.new_header(level=3, title='Let\'s connect ? 🤝')
+    mdFile.new_paragraph(paragraph_init)
+    if linkedin_url is not None:
+        mdFile.new_paragraph(linkedin.format(linkedin_url))
+    if twitter_url is not None:
+        mdFile.new_paragraph(twitter.format(twitter_url))
+    if medium_url is not None:
+        mdFile.new_paragraph(medium.format(medium_url))
+    mdFile.new_paragraph(paragraph_end)
     
     mdFile.create_md_file()
     
