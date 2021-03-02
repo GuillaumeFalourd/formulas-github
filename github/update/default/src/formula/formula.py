cae1baf1ce31a9b1690b99b1101877bd1458c23b
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-02 13:53:47 -0300

    refactor rit github update default
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/update/default/src/formula/formula.py
+++ github/update/default/src/formula/formula.py
@@ -3,16 +3,16 @@ import requests
 import json
 
 def run(token, owner, repository, branch):
-    url = f'https://api.github.com/repos/{owner}/{repository}'
+    url = f"https://api.github.com/repos/{owner}/{repository}"
     
-    authorization = f'token {token}'
+    authorization = f"token {token}"
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization" : authorization,
         }
 
     data = {}
-    data['default_branch'] = branch
+    data["default_branch"] = branch
     
     json_data = json.dumps(data)
     
@@ -23,8 +23,7 @@ def run(token, owner, repository, branch):
     )
         
     if r.status_code == 200:
-        message = "✅ Branch \033[36m%s\033[0m successfully set as default on %s's \033[36m%s\033[0m repository" % (branch, owner, repository)
-        print(message)
+        print(f"✅ Branch \033[36m{branch}\033[0m successfully set as default on {owner}'s \033[36m{repository}\033[0m repository")
 
     else:
         print("❌ Couldn't set the branch as default on the repository")
