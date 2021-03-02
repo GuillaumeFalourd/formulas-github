7a86c2b1f3496cdb52af4d980c954181f5b1e370
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-02 14:06:33 -0300

    refactor rit github generate release
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/generate/release/src/formula/formula.py
+++ github/generate/release/src/formula/formula.py
@@ -4,16 +4,16 @@ import json
 
 def run(token, owner, repository, branch, version, description):
 
-    url = f'https://api.github.com/repos/{owner}/{repository}/releases'
+    url = f"https://api.github.com/repos/{owner}/{repository}/releases"
 
     data = {}
-    data['body'] = description
-    data['target_commitish'] = branch
-    data['tag_name'] = version
-    data['name'] = version
+    data["body"] = description
+    data["target_commitish"] = branch
+    data["tag_name"] = version
+    data["name"] = version
     json_data = json.dumps(data)
 
-    authorization = f'token {token}'
+    authorization = f"token {token}"
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization" : authorization,
@@ -26,8 +26,7 @@ def run(token, owner, repository, branch, version, description):
         )
 
     if r.status_code == 201:
-        message = "✅ Release %s successfully generated for %s's %s repository" % (version, owner, repository)
-        print(message)
+        print(f"✅ Release {version} successfully generated for {owner}'s {repository} repository")
 
     else:
         print("❌ Couldn't generate repository release")
