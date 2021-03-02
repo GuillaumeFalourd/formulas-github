b5e32aa1fc578ce5d545745fe8edcfb325a512ba
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-02 14:19:00 -0300

    refactor rit github get user
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/get/user/src/formula/formula.py
+++ github/get/user/src/formula/formula.py
@@ -4,8 +4,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 import os
 
-api_url_base = 'https://api.github.com/'
-headers = {'Content-Type': 'application/json', 'User-Agent': 'Python Student', 'Accept': 'application/vnd.github.v3+json'}
+api_url_base = "https://api.github.com/"
+headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/vnd.github.v3+json"
+    }
 
 def Run(username, repo_details, keep_file, user, key):
     # Print User details
@@ -22,88 +25,89 @@ def Run(username, repo_details, keep_file, user, key):
     if user_details is not None:
 
         # convert it to utf-8 encoded json string
-        user_in_json = user_details.decode('utf-8')
+        user_in_json = user_details.decode("utf-8")
 
         # Load the JSON to a Python list & dump it back out as formatted JSON
         user_detail_dict = json.loads(user_in_json)
 
-        if user_detail_dict['email'] is None or user_detail_dict['name'] is None:
+        if user_detail_dict["email"] is None or user_detail_dict["name"] is None:
 
             events = requests.get(
-                ('https://api.github.com/users/%s/events?per_page=100' % (username)), auth=HTTPBasicAuth(user, key),
+                url = f"https://api.github.com/users/{username}/events?per_page=100", 
+                auth = HTTPBasicAuth(user, key),
             ).json()
 
-            if user_detail_dict['name'] is None:
-                user_detail_dict['name'] = get_name(events, username)
+            if user_detail_dict["name"] is None:
+                user_detail_dict["name"] = get_name(events, username)
 
-            if user_detail_dict['email'] is None:
-                user_detail_dict['email'] = get_email(events, username, user_detail_dict['name'])
+            if user_detail_dict["email"] is None:
+                user_detail_dict["email"] = get_email(events, username, user_detail_dict["name"])
 
         user_file.write("\n" + "="*10 + " User details of username: " + username + " " + "="*10 + "\n" )
-        user_file.write("🔅 User Name: {}".format(user_detail_dict['name']) + "\n")
-        user_file.write("📔 Bio: {}".format(user_detail_dict['bio']) + "\n")
-        user_file.write("📇 Company: {}".format(user_detail_dict['company']) + "\n")
-        user_file.write("📧 Email: {}".format(user_detail_dict['email']) + "\n")
-        user_file.write("🛰  Location: {}".format(user_detail_dict['location'])+ "\n")
-        user_file.write("👀 Following: {}".format(user_detail_dict['following']) + "\n")
-        user_file.write("👥 Followers: {}".format(user_detail_dict['followers']) + "\n")
-        user_file.write("🔢 Public Repo count: {}".format(user_detail_dict['public_repos']) + "\n")
-        user_file.write("🆙 Account created at: {}".format(user_detail_dict['created_at']) + "\n")
+        user_file.write("🔅 User Name: {}".format(user_detail_dict["name"]) + "\n")
+        user_file.write("📔 Bio: {}".format(user_detail_dict["bio"]) + "\n")
+        user_file.write("📇 Company: {}".format(user_detail_dict["company"]) + "\n")
+        user_file.write("📧 Email: {}".format(user_detail_dict["email"]) + "\n")
+        user_file.write("🛰  Location: {}".format(user_detail_dict["location"])+ "\n")
+        user_file.write("👀 Following: {}".format(user_detail_dict["following"]) + "\n")
+        user_file.write("👥 Followers: {}".format(user_detail_dict["followers"]) + "\n")
+        user_file.write("🔢 Public Repo count: {}".format(user_detail_dict["public_repos"]) + "\n")
+        user_file.write("🆙 Account created at: {}".format(user_detail_dict["created_at"]) + "\n")
     else:
-        print('No User Found')
+        print("❌ No User Found")
 
     if  repo_details == "yes":
         # Print Repo list details
         repo_list = get_repos(username) # It's a binary string
 
         if repo_list is not None:
-            repo_in_json = repo_list.decode('utf-8') # convert it to utf-8 encoded json string
+            repo_in_json = repo_list.decode("utf-8") # convert it to utf-8 encoded json string
 
             # Load the JSON to a Python list & dump it back out as formatted JSON
             repo_list = json.loads(repo_in_json)
             user_file.write("\n" + "="*10 + " Repo details of username: " + username + " " + "="*10 + "\n")
 
             for repo_dict in repo_list:
-                user_file.write("*"*10 + " Repo Name: {}".format(repo_dict['name']) + " " + "*"*10 + "\n")
-                user_file.write("📄 Description: {}".format(repo_dict['description']) + "\n")
-                user_file.write("🌐 Repo url: {}".format(repo_dict['clone_url']) + "\n")
-                user_file.write("🔀 Is it forked one : {}".format(repo_dict['fork']) + "\n")
-                user_file.write("🆕 Created at: {}".format(repo_dict['created_at']) + "\n")
-                user_file.write("🔄 Updated at: {}".format(repo_dict['updated_at']) + "\n")
-                user_file.write("🗣  Language: {}".format(repo_dict['language']) + "\n")
-                user_file.write("🧮 Fork Count: {}".format(repo_dict['forks_count']) + "\n")
+                user_file.write("*"*10 + " Repo Name: {}".format(repo_dict["name"]) + " " + "*"*10 + "\n")
+                user_file.write("📄 Description: {}".format(repo_dict["description"]) + "\n")
+                user_file.write("🌐 Repo url: {}".format(repo_dict["clone_url"]) + "\n")
+                user_file.write("🔀 Is it forked one : {}".format(repo_dict["fork"]) + "\n")
+                user_file.write("🆕 Created at: {}".format(repo_dict["created_at"]) + "\n")
+                user_file.write("🔄 Updated at: {}".format(repo_dict["updated_at"]) + "\n")
+                user_file.write("🗣  Language: {}".format(repo_dict["language"]) + "\n")
+                user_file.write("🧮 Fork Count: {}".format(repo_dict["forks_count"]) + "\n")
                 user_file.write("\n")
         else:
-            print('No Repo List Found')
+            print('❌ No Repo List Found')
 
     user_file.close()
 
-    f = open(file_name, 'r')
+    f = open(file_name, "r")
     file_contents = f.read()
     print (file_contents)
     f.close()
 
     if keep_file == "no":
-        os.system(f'rm -rf {file_name}')
+        os.system(f"rm -rf {file_name}")
 
 def get_user_details(username):
-    user_url = '{}users/{}'.format(api_url_base, username)
+    user_url = f"{api_url_base}users/{username}"
     response = requests.get(user_url, headers=headers)
 
     if response.status_code == 200:
         return response.content
     else:
-        print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url_base))
+        print(f"[!] HTTP {response.status_code} calling [{user_url}]")
         return None
 
 def get_repos(username):
-    repo_url = '{}users/{}/repos'.format(api_url_base, username)
+    repo_url = f"{api_url_base}users/{username}/repos"
     response = requests.get(repo_url, headers=headers)
 
     if response.status_code == 200:
         return (response.content)
     else:
-        print('[!] HTTP {0} calling [{1}]'.format(response.status_code, api_url_base))
+        print(f"[!] HTTP {response.status_code} calling [{repo_url}]")
         return None
 
 def get_name(events, username):
