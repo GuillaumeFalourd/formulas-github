2f5c4df8d5c08a1e9988d8bdafa95ee85ab03491
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-02 14:00:04 -0300

    refactor rit github delete collaborator
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/delete/collaborator/src/formula/formula.py
+++ github/delete/collaborator/src/formula/formula.py
@@ -4,35 +4,33 @@ import json
 
 def run(token, username, repository, collaborator):
 
-    authorization = f'token {token}'
+    url = f"https://api.github.com/repos/{username}/{repository}/collaborators{collaborator}"
+
+    authorization = f"token {token}"
     headers = {
         "Accept": "application/vnd.github.v3+json",
         "Authorization" : authorization,
         }
 
-    url = f'  https://api.github.com/repos/{username}/{repository}/collaborators{collaborator}'
-
     r1 = requests.get(
         url=url,
         headers=headers
-        )
+    )
 
     if r1.status_code == 204:
 
+        r2 = requests.delete(
+            url=url,
+            headers=headers
+        )
 
-            r2 = requests.delete(
-                url=url,
-                headers=headers
-                )
+        if r2.status_code == 204:
+            message = f"Collaborator {collaborator} successfully removed from {username}'s {repository} repository"
+            print(message)
 
-            if r2.status_code == 204:
-                message = "Collaborator %s successfully removed from %s repository" % (collaborator, repository)
-                print(message)
-
-            else:
-                print("Couldn't delete the collaborator from the repository")
-                print (r2.status_code, r2.reason)
+        else:
+            print("Couldn't delete the collaborator from the repository")
+            print (r2.status_code, r2.reason)
 
     else:
-        message = "Username %s isn't a %s repository collaborator" % (collaborator, repository)
-        print(message)
+        print(f"Username {collaborator} isn't a {repository} repository collaborator")
