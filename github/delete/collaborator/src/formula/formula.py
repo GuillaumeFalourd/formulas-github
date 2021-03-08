bc3fc6290c2c989b88c41014a08be07e74f9d93d
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-08 17:22:24 -0300

    update rit github delete colaborator formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/delete/collaborator/src/formula/formula.py
+++ github/delete/collaborator/src/formula/formula.py
@@ -4,7 +4,7 @@ import json
 
 def run(token, username, repository, collaborator):
 
-    url = f"https://api.github.com/repos/{username}/{repository}/collaborators{collaborator}"
+    url = f"https://api.github.com/repos/{username}/{repository}/collaborators/{collaborator}"
 
     authorization = f"token {token}"
     headers = {
@@ -18,7 +18,7 @@ def run(token, username, repository, collaborator):
         )
 
     if r1.status_code == 204:
-
+        
         r2 = requests.delete(
             url=url,
             headers=headers
