25804eef8c6fadbc4f2cf20c17a553343011d296
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-20 17:05:13 -0300

    refactor rit github update repo default-branch formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/update/repo/default-branch/src/formula/formula.py
+++ github/update/repo/default-branch/src/formula/formula.py
@@ -4,7 +4,7 @@ import json
 
 def run(token, owner, repository, branch):
     url = f"https://api.github.com/repos/{owner}/{repository}"
-    
+
     authorization = f"token {token}"
     headers = {
         "Accept": "application/vnd.github.v3+json",
@@ -13,15 +13,15 @@ def run(token, owner, repository, branch):
 
     data = {}
     data["default_branch"] = branch
-    
+
     json_data = json.dumps(data)
-    
+
     r = requests.post(
         url=url,
         data=json_data,
         headers=headers
     )
-        
+
     if r.status_code == 200:
         print(f"✅ Branch \033[36m{branch}\033[0m successfully set as default on {owner}'s \033[36m{repository}\033[0m repository")
 
