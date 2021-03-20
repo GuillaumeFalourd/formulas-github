5fb73c869440d6fd27e7a2d073cc618924ffbb02
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-20 16:59:34 -0300

    refactor rit github delete repo formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/delete/repo/src/formula/formula.py
+++ github/delete/repo/src/formula/formula.py
@@ -5,7 +5,7 @@ import json
 import re
 
 def run(token, username):
-    
+
     url = f"https://api.github.com/user/repos?type=owner&per_page=100&sort=full_name"
 
     authorization = f"token {token}"
@@ -13,7 +13,7 @@ def run(token, username):
         "Accept": "application/vnd.github.v3+json",
         "Authorization" : authorization,
         }
-    
+
     r1 = requests.get(
         url=url,
         headers=headers
@@ -22,10 +22,10 @@ def run(token, username):
     if r1.status_code == 200:
         datas = r1.json()
         repositories = []
-    
+
         for d in datas:
             repositories.append(d["name"])
-            
+
         question1 = [
             inquirer.List("repository",
                     message = f"\033[1m\033[36m{username}\033[0m \033[1mrepository to delete:\033[0m ",
@@ -34,7 +34,7 @@ def run(token, username):
         ]
         answer = inquirer.prompt(question1)
         repository = answer["repository"]
-        
+
         question2 = [
             inquirer.List("confirmation",
                     message = f"\033[1mAre you sure you want to delete \033[36m{repository}\033[0m \033[1mrepo?\033[0m",
@@ -43,7 +43,7 @@ def run(token, username):
         ]
         answer = inquirer.prompt(question2)
         confirmation = answer["confirmation"]
-        
+
         if confirmation == "Yes":
             url = f"https://api.github.com/repos/{username}/{repository}"
 
@@ -58,7 +58,7 @@ def run(token, username):
             else:
                 print(f"❌ Couldn't delete repository https://github.com/{username}/{repository}")
                 print(r2.status_code, r2.reason)
-    
+
     if r1.status_code != 200:
         print(f"❌ Couldn't get {username}'s repositories")
         print(r1.status_code, r1.reason)
--- github/delete/repo/src/main.py
+++ github/delete/repo/src/main.py
@@ -3,7 +3,7 @@ import os
 
 from formula import formula
 
-token = os.environ.get("RIT_GIT_TOKEN")
-username = os.environ.get("RIT_GIT_USER")
+token = os.environ.get("RIT_GITHUB_TOKEN")
+username = os.environ.get("RIT_GITHUB_USER")
 
 formula.run(token, username)
