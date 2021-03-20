847db2d57b7694afcc6bafc775d74eaef4e54d1e
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-03-20 17:01:36 -0300

    refactor rit github get repo formula
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- github/get/repo/src/main.py
+++ github/get/repo/src/main.py
@@ -3,8 +3,8 @@ import os
 
 from formula import formula
 
-user = os.environ.get("GIT_USERNAME")
-key = os.environ.get("GIT_TOKEN")
+user = os.environ.get("RIT_GITHUB_USER")
+key = os.environ.get("RIT_GITHUB_TOKEN")
 repository = os.environ.get("RIT_GITHUB_REPOSITORY")
 contribution = os.environ.get("CONTRIBUTION")
 
